cab8de17aac92f5ff4d55c1f6ab612a0d4449159
Aivanas <vk53418006@gmail.com>
2023-04-23 17:52:14 +0300

    пт

--- MegaEzhednevnich/MainWindow.xaml.cs
+++ MegaEzhednevnich/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.MessageBox;
 
 namespace MegaEzhednevnich
 {
@@ -20,9 +24,171 @@ namespace MegaEzhednevnich
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool is_edited_name = false;
+        bool is_edited_description = false;
+        List<Notes> notes = new List<Notes>();
+        List<Notes> today_notes = new List<Notes>();
         public MainWindow()
         {
+            try
+            {
+                DateTime now = DateTime.Today;              
+                UpdateList(now);
+            }
+            catch
+            {
+                MessageBox.Show("OBKAKISH ALARM!!!");
+            }
+
             InitializeComponent();
+        }
+
+        public void UpdateList(DateTime date)
+        {    
+            try
+            {
+                today_notes.Clear();
+                notes = Notes.Deserialize(notes);
+            }
+            catch 
+            {
+                MessageBox.Show(" DESERIALIZATION OBKAKISH ALARM!!!");
+            }
+            if (NotesList != null)
+            {
+                NotesList.Items.Clear();
+            }
+            
+            
+            foreach (Notes one_note in notes)
+            {
+                if (one_note.date == date)
+                {
+                    NotesList.Items.Add(one_note.name);   
+                    today_notes.Add(one_note);
+                }
+            }
+            
+                                    
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                try
+                {
+                    if (notes[i].name == NotesList.SelectedValue.ToString() &
+                    notes[i].date == DateChooser.SelectedDate.Value)
+                    // если здесь ошибка, то запускайте без отладки и все будет хорошо 
+                    {
+                        Notes note = notes[i];
+                        note.date = DateChooser.SelectedDate.Value;
+                        note.name = NoteName.Text;
+                        note.description = NoteDescription.Text;
+                        notes[i] = note;
+                        Notes.Serialize(notes);
+                        UpdateList(DateChooser.SelectedDate.Value);
+                        DeleteButtton.IsEnabled = false;
+                        SaveButton.IsEnabled = false;
+                    }
+                }
+                catch
+                {
+                    continue;
+                    //Зачем напрягаться если можно не напрягаться
+                }
+            }
+        }
+
+        private void NoteDescription_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!is_edited_description)
+            {
+                NoteDescription.Clear();
+                NoteDescription.Foreground = Brushes.Black;
+                NoteDescription.FontStyle = FontStyles.Normal;
+                is_edited_description = true;
+            }
+        }
+
+        private void NoteName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {          
+            if (!is_edited_name)
+            {
+                NoteName.Clear();
+                NoteName.Foreground = Brushes.Black;
+                NoteName.FontStyle = FontStyles.Normal;
+                is_edited_name = true;
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Notes note = new Notes();
+            note.date = DateChooser.SelectedDate.Value;
+            note.name = NoteName.Text;
+            note.description = NoteDescription.Text;
+            notes.Add(note);
+            Notes.Serialize(notes);            
+            UpdateList(DateChooser.SelectedDate.Value);
+        }
+
+        private void DateChooser_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            NotesList.Items.Clear();
+            UpdateList(DateChooser.SelectedDate.Value);
+                   
+        }
+
+        private void NotesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Notes choosed_note = new Notes();
+            foreach (Notes one_note in today_notes)
+            {
+                if (one_note.name == NotesList.SelectedValue.ToString())
+                {
+                    choosed_note = one_note;
+                }
+            }
+            is_edited_description = true;
+            is_edited_name = true;
+            NoteName.Text = choosed_note.name;
+            NoteName.Foreground = Brushes.Black;
+            NoteName.FontStyle = FontStyles.Normal;
+            NoteDescription.Text = choosed_note.description;
+            NoteDescription.Foreground = Brushes.Black;
+            NoteDescription.FontStyle = FontStyles.Normal;
+
+            SaveButton.IsEnabled = true;
+            DeleteButtton.IsEnabled = true; 
+        }
+
+        private void DeleteButtton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i<notes.Count; i++)
+            {   
+                try
+                {
+                    if (notes[i].name == NotesList.SelectedValue.ToString() &
+                    notes[i].date == DateChooser.SelectedDate.Value)
+                    // если здесь ошибка, то запускайте без отладки и все будет хорошо 
+                    {
+                        notes.Remove(notes[i]);
+                        Notes.Serialize(notes);
+                        UpdateList(DateChooser.SelectedDate.Value);
+                        DeleteButtton.IsEnabled = false;
+                        SaveButton.IsEnabled=false;
+                    }
+                }
+                catch 
+                {
+                    continue;
+                    //Зачем напрягаться если можно не напрягаться
+                }
+                
+            }
         }
     }
 }
---
+++ MegaEzhednevnich/Notes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Newtonsoft.Json;
+using Xceed.Wpf.Toolkit;
+
+namespace MegaEzhednevnich
+{
+    internal class Notes
+    {
+        public string name;
+        public string description;
+        public DateTime date;       
+
+        public static void Serialize(List<Notes> notes)
+        {
+            string json_text = JsonConvert.SerializeObject(notes);
+            File.WriteAllText("Notes.json", json_text);
+
+        }
+
+        public static List<Notes> Deserialize(List<Notes> notes)
+        {
+            string json_text = File.ReadAllText("Notes.json");
+            notes = JsonConvert.DeserializeObject<List<Notes>>(json_text);
+            List<Notes> result = new List<Notes>();
+            foreach (Notes note in notes)
+            {
+                Notes one_notes = new Notes();
+                one_notes.name = note.name;
+                one_notes.description = note.description;
+                one_notes.date = note.date;
+                result.Add(one_notes);
+            }
+            return result;
+        }
+    }
+
+}
